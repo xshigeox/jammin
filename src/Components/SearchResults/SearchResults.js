be185958e0335ba7b91c2f4f78d078f7dbbe85bd
xshigeox <59002383+xshigeox@users.noreply.github.com>
2020-06-30 14:32:48 -0400

    pass props to tracklist

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,10 +1,12 @@
 import React from "react"
+import "./SearchResults.css"
+import TrackList from "../TrackList/TrackList"
 
 const SearchResults = (props) => {
   return (
     <div className="SearchResults">
       <h2>Results</h2>
-      {/* <!-- Add a TrackList component --> */}
+      <TrackList tracks={props.searchResults} />
     </div>
   )
 }
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,10 +2,12 @@ import React from "react"
 
 const Track = (props) => {
   return (
-    <div class="Track">
-      <div class="Track-information">
-        {/* <h3><!-- track name will go here --></h3> */}
-        {/* <p><!-- track artist will go here--> | <!-- track album will go here --></p> */}
+    <div className="Track">
+      <div className="Track-information">
+        <h3>{props.track.name}</h3>
+        <p>
+          {props.track.artist} | {props.track.album}
+        </p>
       </div>
       {/* <button class="Track-action"><!-- + or - will go here --></button> */}
     </div>
